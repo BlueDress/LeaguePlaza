dd3ca0c37bdd131c7a16d8646dcdac155cb6c6e5
Blue Dress <stra6aro@abv.bg>
2025-01-25 21:58:24 +0200

    added utils.js

--- LeaguePlaza.Web/wwwroot/js/quest/view-quest.js
+++ LeaguePlaza.Web/wwwroot/js/quest/view-quest.js
@@ -43,15 +43,4 @@ function viewQuestMain() {
             }
         }
     }
-
-    function createElement(tag, text, attributes) {
-        const element = document.createElement(tag);
-        element.innerText = text;
-
-        for (const [key, value] of Object.entries(attributes)) {
-            element.setAttribute(key, value);
-        }
-
-        return element;
-    }
 }
