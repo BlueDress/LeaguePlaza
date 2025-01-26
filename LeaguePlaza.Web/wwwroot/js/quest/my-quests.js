5c390ce0711bdfd1f83454db719a66aac5bc3f8a
Blue Dress <stra6aro@abv.bg>
2025-01-26 23:28:49 +0200

    added form validation

--- LeaguePlaza.Web/wwwroot/js/quest/my-quests.js
+++ LeaguePlaza.Web/wwwroot/js/quest/my-quests.js
@@ -3,10 +3,12 @@
 function myQuestsMain() {
     const baseUrl = '/api/questapi/';
 
+    const inputs = document.querySelectorAll('input');
+
     const titleInput = document.querySelector('#title');
     const descriptionInput = document.querySelector('#description');
     const rewardInput = document.querySelector('#reward');
-    const typeInput = document.querySelector('#type');
+    const typeSelect = document.querySelector('#type');
 
     const createQuestForm = document.querySelector('#create-quest');
     const updateBtn = document.querySelector('#update-quest-btn');
@@ -14,45 +16,96 @@ function myQuestsMain() {
 
     let questId;
 
+    inputs?.forEach(i => i.addEventListener('blur', e => handleFormElementInput(e.target)));
+    typeSelect?.addEventListener('blur', e => handleFormElementInput(e.target));
     createQuestForm?.addEventListener('submit', e => createQuest(e));
     updateBtn?.addEventListener('click', e => updateQuest(e));
     questsHolder.addEventListener('click', e => handleQuestButtonClick(e));
 
+    function handleFormElementInput(element) {
+        const span = element.nextElementSibling;
+
+        if (elementInputIsValid(element)) {
+            hideErrorMessage(span);
+            return;
+        }
+
+        displayErrorMessage(span, `${element.name} is required`);
+    }
+
+    function elementInputIsValid(element) {
+        return !element.hasAttribute('required') || element.value;
+    }
+
+    function displayErrorMessage(element, message) {
+        element.classList.add('error-message');
+        element.classList.remove('display-none');
+        element.innerText = message;
+    }
+
+    function hideErrorMessage(element) {
+        element.classList.remove('error-message');
+        element.classList.add('display-none');
+        element.innerText = '';
+    }
+
     async function createQuest(e) {
         e.preventDefault();
 
-        const newQuest = {
-            title: titleInput.value,
-            description: descriptionInput.value,
-            rewardAmount: rewardInput.value,
-            type: typeInput.value
-        }
+        if (formIsValid([titleInput, rewardInput, typeSelect])) {
+            const newQuest = {
+                title: titleInput.value,
+                description: descriptionInput.value,
+                rewardAmount: rewardInput.value,
+                type: typeSelect.value
+            }
 
-        const response = await fetch(baseUrl + 'createquest', {
-            method: 'POST',
-            headers: {
-                'content-type': 'application/json',
-            },
-            body: JSON.stringify(newQuest),
-        });
+            const response = await fetch(baseUrl + 'createquest', {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json',
+                },
+                body: JSON.stringify(newQuest),
+            });
+        }
     }
 
     async function updateQuest(e) {
-        const questToUpdate = {
-            id: questId,
-            title: titleInput.value,
-            description: descriptionInput.value,
-            rewardAmount: rewardInput.value,
-            type: typeInput.value
+        if (formIsValid([titleInput, rewardInput, typeSelect])) {
+            const questToUpdate = {
+                id: questId,
+                title: titleInput.value,
+                description: descriptionInput.value,
+                rewardAmount: rewardInput.value,
+                type: typeSelect.value
+            }
+
+            const response = await fetch(baseUrl + 'updatequest', {
+                method: 'PUT',
+                headers: {
+                    'content-type': 'application/json',
+                },
+                body: JSON.stringify(questToUpdate),
+            });
+        }
+    }
+
+    function formIsValid(elements) {
+        let formIsValid = true;
+
+        for (const element of elements) {
+            const span = element.nextElementSibling;
+
+            if (elementInputIsValid(element)) {
+                hideErrorMessage(span);
+                break;
+            }
+
+            displayErrorMessage(span, `${element.name} is required`);
+            formIsValid = false;
         }
 
-        const response = await fetch(baseUrl + 'updatequest', {
-            method: 'PUT',
-            headers: {
-                'content-type': 'application/json',
-            },
-            body: JSON.stringify(questToUpdate),
-        });
+        return formIsValid;
     }
 
     async function handleQuestButtonClick(e) {
@@ -74,7 +127,7 @@ function myQuestsMain() {
         titleInput.value = ul.children[1].textContent;
         descriptionInput.value = ul.children[2].textContent;
         rewardInput.value = parseFloat(ul.children[4].textContent.replace(',', '.'));
-        typeInput.value = getQuestTypeValue(ul.children[5].textContent);
+        typeSelect.value = getQuestTypeValue(ul.children[5].textContent);
         questId = ul.children[0].textContent;
     }
 
