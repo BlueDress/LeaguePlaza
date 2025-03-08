62ef4c5f6dcb49cdf3b9eb06f44b302ca4fc92d9
Blue Dress <stra6aro@abv.bg>
2025-03-08 21:06:47 +0200

    pagination view model added

---
+++ LeaguePlaza.Core/Features/Pagination/Models/PaginationViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace LeaguePlaza.Core.Features.Pagination.Models
+{
+    public class PaginationViewModel
+    {
+        public int CurrentPage { get; set; }
+
+        public int TotalPages { get; set; }
+    }
+}
