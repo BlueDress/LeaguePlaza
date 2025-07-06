b2d23896841517246c2f015fa1d538a1e93a1ff0
Blue Dress <stra6aro@abv.bg>
2025-07-06 16:34:43 +0300

    added mount and products cards to home page

--- LeaguePlaza.Common/Constants/PaginationConstants.cs
+++ LeaguePlaza.Common/Constants/PaginationConstants.cs
@@ -9,8 +9,10 @@
         public const int QuestsForHomePage = 3;
         public const int QuestsPerPage = 6;
 
+        public const int ProductsForHomePage = 3;
         public const int ProductsPerPage = 6;
 
+        public const int MountsForHomePage = 3;
         public const int MountsPerPage = 6;
         public const int MountRentalsPerPage = 10;
 
--- LeaguePlaza.Core/Features/Home/Services/HomeService.cs
+++ LeaguePlaza.Core/Features/Home/Services/HomeService.cs
@@ -1,10 +1,14 @@
 ﻿using LeaguePlaza.Core.Features.Home.Contracts;
 using LeaguePlaza.Core.Features.Home.Models.ViewModels;
 using LeaguePlaza.Core.Features.Quest.Models.Dtos.ReadOnly;
+using LeaguePlaza.Core.Features.Mount.Models.Dtos.ReadOnly;
+using LeaguePlaza.Core.Features.Product.Models.Dtos.ReadOnly;
 using LeaguePlaza.Infrastructure.Data.Entities;
 using LeaguePlaza.Infrastructure.Data.Repository;
 
 using static LeaguePlaza.Common.Constants.QuestConstants;
+using static LeaguePlaza.Common.Constants.MountConstants;
+using static LeaguePlaza.Common.Constants.ProductConstants;
 using static LeaguePlaza.Common.Constants.PaginationConstants;
 
 namespace LeaguePlaza.Core.Features.Home.Services
@@ -15,11 +19,13 @@ namespace LeaguePlaza.Core.Features.Home.Services
 
         public async Task<HomePageViewModel> CreateHomePageViewModelAsync()
         {
-            var latestQuests = await _repository.FindSpecificCountOrderedReadOnlyAsync<QuestEntity, DateTime>(PageOne, QuestsForHomePage, true, q => q.Created, q => q.AdventurerId == null);
+            IEnumerable<QuestEntity> latestQuests = await _repository.FindSpecificCountOrderedReadOnlyAsync<QuestEntity, DateTime>(PageOne, QuestsForHomePage, true, q => q.Created, q => q.AdventurerId == null);
+            IEnumerable<MountEntity> latestMounts = await _repository.FindSpecificCountOrderedReadOnlyAsync<MountEntity, DateTime>(PageOne, MountsForHomePage, true, m => m.Created, m => true);
+            IEnumerable<ProductEntity> cheapestProducts = await _repository.FindSpecificCountOrderedReadOnlyAsync<ProductEntity, decimal>(PageOne, ProductsForHomePage, false, p => p.Price, p => p.IsInStock);
 
             return new HomePageViewModel()
             {
-                LatestQuests = latestQuests.Select(q => new QuestDto
+                LatestQuests = latestQuests.Select(q => new QuestDto()
                 {
                     Id = q.Id,
                     Title = q.Title,
@@ -28,6 +34,25 @@ namespace LeaguePlaza.Core.Features.Home.Services
                     RewardAmount = q.RewardAmount,
                     Type = q.Type.ToString(),
                     ImageUrl = q.ImageName,
+                }),
+                LatestMounts = latestMounts.Select(m => new MountDto()
+                {
+                    Id = m.Id,
+                    Name = m.Name,
+                    Description = string.IsNullOrWhiteSpace(m.Description) ? NoMountDescriptionAvailable : m.Description,
+                    RentPrice = m.RentPrice,
+                    ImageUrl = m.ImageUrl,
+                    Type = m.MountType.ToString(),
+                    Rating = m.Rating,
+                }),
+                CheapestProducts = cheapestProducts.Select(p => new ProductDto()
+                {
+                    Id = p.Id,
+                    Name = p.Name,
+                    Description = string.IsNullOrWhiteSpace(p.Description) ? NoProductDescriptionAvailable : p.Description,
+                    Price = p.Price,
+                    ImageUrl = p.ImageUrl,
+                    ProductType = p.ProductType.ToString(),
                 }),
             };
         }
