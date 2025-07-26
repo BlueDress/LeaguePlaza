c340260a4e1095890c492c2af4215c261825e145
Blue Dress <stra6aro@abv.bg>
2025-07-26 20:27:36 +0300

    added sub menu for header navigation

--- LeaguePlaza.Web/wwwroot/js/navigation.js
+++ LeaguePlaza.Web/wwwroot/js/navigation.js
@@ -6,7 +6,10 @@ function navigationMain() {
     const hamburgerMenu = document.querySelector('#hamburger-menu');
     const cartCountBadge = document.querySelector('.cart-nav-icon > span');
 
+    const navSubMenuArrows = document.querySelectorAll('.nav-sub-menu-arrow-js');
+
     hamburgerMenu.addEventListener('click', e => handleHamburgerMenuClick(e));
+    navSubMenuArrows.forEach(a => a.addEventListener('click', e => handleNavSubMenuArrowClick(e)));
 
     if (cartCountBadge) {
         updateCartCountBadge();
@@ -36,5 +39,23 @@ function navigationMain() {
         if (response.status == 200) {
             cartCountBadge.textContent = await response.text();
         }
+    }
+
+    function handleNavSubMenuArrowClick(e) {
+        e.preventDefault();
+
+        const navSubLinks = e.target.closest('li').querySelector('.nav-sub-links-js');
+        const arrows = e.target.closest('a').querySelectorAll('.nav-sub-menu-arrow-js');
+
+        if (navSubLinks.classList.contains('open')) {
+            navSubLinks.classList.remove('open');
+        } else {
+            document.querySelectorAll('.nav-sub-links-js').forEach(el => el.classList.remove('open'));
+            document.querySelectorAll('.fa-chevron-down').forEach(el => el.parentElement.classList.remove('display-none'));
+            document.querySelectorAll('.fa-chevron-up').forEach(el => el.parentElement.classList.add('display-none'));
+            navSubLinks.classList.add('open');
+        }
+
+        arrows.forEach(a => a.classList.toggle('display-none'));
     }
 }
